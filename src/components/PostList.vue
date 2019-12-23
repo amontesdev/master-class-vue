8c08647ec4362f98d790bbfd2f6e8a4e9366e5b7
Andres Montes <amontes@merqueo.com>
2019-12-23 14:23:58 -0500

    add postList and PostListItem components

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list">
+      <PostListItem 
+        v-for="post in posts"
+        :post="post"
+        :key="post['.key']"
+      />
+  </div>
+</template>
+<script>
+import PostListItem from 'comp/PostListItem'
+export default {
+  components: {
+    PostListItem
+  },
+  props: {
+    posts: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+<style module>
+
+</style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,27 +2,9 @@
   <div class="col-large push-top">
     <div>
       <h2>{{thread.title}}</h2>
-      <div class="post-list">
-        <div class="post" v-for="postId in thread.posts" :key="postId">
-          <div class="user-info">
-            <a href="#" class="username">{{users[posts[postId].userId].name}}</a>
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-            </a>
-            <p class="desktop-only text-small"></p>
-          </div>
-          <div class="post-content">
-            <div>
-              <p>
-                {{posts[postId].text}}
-              </p>
-            </div>
-          </div>
-          <div class="post-date text-faded">
-
-          </div>
-        </div>
-      </div>
+      <PostList 
+        :posts="posts"
+      />
     </div>
   </div>
  
@@ -30,7 +12,11 @@
 
 <script>
 import sourceData from '@/data.json'
+import PostList from 'comp/PostList'
 export default {
+  components: {
+    PostList
+  },
   props: {
     id: {
       required: true,
@@ -42,6 +28,13 @@ export default {
       thread: sourceData.threads[this.id],
       posts: sourceData.posts,
       users: sourceData.users
+    }
+  },
+  computed: {
+    posts () {
+      const postIds = Object.values(this.thread.posts)
+      return Object.values(sourceData.posts)
+        .filter(post => postIds.includes(post['.key']))
     }
   }
 }
